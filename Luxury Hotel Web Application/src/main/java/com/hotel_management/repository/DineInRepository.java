0b2482e30666e4fb55524e1585aac6a0b93114e8
Ayush Torgalmath <ayushtorgalmath@gmail.com>
2026-04-28 22:47:55 +0530

    Delete Luxury Hotel Web Application directory

--- Luxury Hotel Web Application/src/main/java/com/hotel_management/repository/DineInRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hotel_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotel_management.model.DineIn;
-
-public interface DineInRepository extends JpaRepository<DineIn, Integer> {
-}
